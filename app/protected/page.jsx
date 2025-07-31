f7a4a8531caff1b5326147342178d529e5dc60ac
Kaipher <Simmigon.flagg@gmail.com>
2025-07-31 00:57:08 -0400

    Update page UI

--- app/protected/page.jsx
+++ app/protected/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>This page is protected</div>
+
+<div className="min-h-screen flex flex-col">
+  <div className="flex-1 flex flex-col w-full h-full bg-gray-100 dark:bg-gray-800 p-10 text-4xl font-bold mb-6 items-center">
+    This page is protected
+  </div>
+</div>
   )
 }
 
